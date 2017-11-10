e43008f353af4d8494c53b9f33e39edd1ec4d702
Marjana-Tbox <31036857+Marjana-Tbox@users.noreply.github.com>
2017-11-10 15:43:11 +0000

    remove app usage from view

--- Jisc/MenuButton.swift
+++ Jisc/MenuButton.swift
@@ -170,7 +170,7 @@ class StatsMenuButton: MenuButton,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var statsMenuButtonsTable: UITableView!
     
 	var expanded = false
-    var menuItemsArray = [localized("activity_points"),localized("app_usage"),localized("attainment"),localized("attendence"),localized("vle_activity")]
+    var menuItemsArray = [localized("activity_points"),localized("attainment"),localized("attendence"),localized("vle_activity")]
     
 	override func buttonAction(_ sender: UIButton?) {
 		if expanded {
@@ -188,7 +188,7 @@ class StatsMenuButton: MenuButton,UITableViewDelegate,UITableViewDataSource {
         self.statsMenuButtonsTable.register(UITableViewCell.self, forCellReuseIdentifier: "MenuCell")
 		UIView.animate(withDuration: 0.25) {
 			self.arrow.transform = CGAffineTransform(rotationAngle: .pi / 2.0)
-			self.buttonsHeight.constant = 40 * 5 //This constant multiplication multiplies the height by the number of buttons shown, for example 40 * 4(buttons) or 40 *6(buttons) Adjust it as necesary.
+			self.buttonsHeight.constant = 40 * 4 //This constant multiplication multiplies the height by the number of buttons shown, for example 40 * 4(buttons) or 40 *6(buttons) Adjust it as necesary.
 			self.parent?.layoutIfNeeded()
 		}
         var result = ""
